fa36c666ff549d06f905ed84ad3892f0829e53af
George Patterson <georgelpatterson2002@gmail.com>
2024-10-30 13:33:02 -0700

    Adding new coin values

--- Assets/General Scripts/Coin.cs
+++ Assets/General Scripts/Coin.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int coinValue = 1; // Value of the coin
+    public float hoverSpeed = 1f; // Speed of the hovering motion
+    public float hoverHeight = 0.2f; // Height of the hovering motion
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        // Record the starting position of the coin
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Make the coin hover up and down
+        float newY = startPosition.y + Mathf.Sin(Time.time * hoverSpeed) * hoverHeight;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //Debug.Log("Coin collected!")
             CurrencyHUD currencyHUD = FindObjectOfType<CurrencyHUD>();
             if (currencyHUD != null)
             {
