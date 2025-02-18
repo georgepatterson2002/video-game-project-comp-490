21cc66b64f9dc5aec5d4790cd23bec0093614791
Randy Reyes Barrios <randyrrb0@gmail.com>
2025-02-17 23:40:09 -0800

    Scene Change
    -scene change
    -crosshair change
    -static player with gun
    -background mini game 1

---
+++ Assets/General Scripts/Start_MiniGame1.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPromptTriggerToMiniGame1 : MonoBehaviour
+{
+    public GameObject startPromptCanvas; 
+    private bool isPlayerInArea = false; 
+
+    private void Start()
+    {
+
+        if (startPromptCanvas != null)
+        {
+            startPromptCanvas.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        // When player is in area and presses 'E'
+        if (isPlayerInArea && Input.GetKeyDown(KeyCode.E))
+        {
+            // Enter code to start the game
+            Debug.Log("Game Starting...");
+            SceneManager.LoadScene("Mini_Game1");
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the player has entered the trigger zone
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInArea = true;
+            if (startPromptCanvas != null)
+            {
+                startPromptCanvas.SetActive(true); // Show "Press E" prompt
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the player has exited the trigger zone
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInArea = false;
+            if (startPromptCanvas != null)
+            {
+                startPromptCanvas.SetActive(false); // Hide "Press E" prompt
+            }
+        }
+    }
+}
