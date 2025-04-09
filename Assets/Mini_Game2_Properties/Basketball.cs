91fb263b1c9330c61d1b2a489da22ef34da74708
Randy Reyes Barrios <randy.reyes.605@my.csun.edu>
2025-04-09 03:32:56 -0700

    Updated MiniGame2
    Better Ball movement

--- Assets/Mini_Game2_Properties/Basketball.cs
+++ Assets/Mini_Game2_Properties/Basketball.cs
@@ -2,10 +2,59 @@ using UnityEngine;
 
 public class Basketball : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    public float maxForce = 20f;
+
+    private Rigidbody2D rb;
+    private Vector2 dragStart;
+    private bool isDragging = false;
+    private bool hasShot = false;
+    private BasketballShoot spawner;
+
+    void Start()
     {
-        if (other.gameObject.CompareTag("Ground"))
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        spawner = FindObjectOfType<BasketballShoot>();
+    }
+
+    void Update()
+    {
+        if (hasShot) return;
+
+        if (Input.GetMouseButtonDown(0))
         {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D hit = Physics2D.OverlapPoint(mousePos);
+            if (hit != null && hit.gameObject == gameObject)
+            {
+                isDragging = true;
+                dragStart = mousePos;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && isDragging)
+        {
+            Vector2 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = currentMousePos;
+        }
+
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+            hasShot = true;
+            rb.isKinematic = false;
+
+            Vector2 dragEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 force = (dragStart - dragEnd).normalized * Mathf.Min(Vector2.Distance(dragStart, dragEnd), 5f) * maxForce;
+            rb.AddForce(force, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            spawner.NotifyBallDestroyed();
             Destroy(gameObject);
         }
     }
