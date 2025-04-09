789ab51ca8e2ce06d7fd081f67eb3cb293b55c14
Randy Reyes Barrios <randy.reyes.605@my.csun.edu>
2025-04-09 03:14:08 -0700

    Updated MiniGame 2
    made it 2d version

---
+++ Assets/Mini_Game2_Properties/Basketball.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Basketball : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
